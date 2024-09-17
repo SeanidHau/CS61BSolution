6a619bb761c6efba9b8a625b9a618d10607bd3f1
He Tianye <he413260@163.com>
2024-09-18 00:41:47 +0800

    Add hw1 basic part

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,34 @@
+package synthesizer;
+
+public interface BoundedQueue<T> {
+    /** return size of the buffer. */
+    int capacity();
+
+    /** return number of items currently in the buffer. */
+    int fillCount();
+
+    /** add item x to the end. */
+    void enqueue(T x);
+
+    /** delete and return item from the front. */
+    T dequeue();
+
+    /** return (but do not delete) item from the front. */
+    T peek();
+
+    /** is the buffer empty (fillCount equals zero)? */
+    default boolean isEmpty() {
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** is the buffer full (fillCount is same as capacity)? */
+    default boolean isFull() {
+        if (fillCount() == capacity()) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ hw1/synthesizer/GuitarString.java
@@ -0,0 +1,48 @@
+package synthesizer;
+
+
+public class GuitarString {
+    /** Constants. Do not change. In case you're curious, the keyword final means
+     * the values cannot be changed at runtime. We'll discuss this and other topics
+     * in lecture on Friday. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = .996; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private BoundedQueue<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public GuitarString(double frequency) {
+        int capicity = (int) Math.round(SR / frequency);
+        buffer = new ArrayRingBuffer<>(capicity);
+        for (int i = 0; i < capicity; i++) {
+            buffer.enqueue(0.0);
+        }
+    }
+
+
+    /* Pluck the guitar string by replacing the buffer with white noise. */
+    public void pluck() {
+        while (!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        while (!buffer.isFull()) {
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm. 
+     */
+    public void tic() {
+        double front = buffer.dequeue();
+        double back = buffer.peek();
+        buffer.enqueue(DECAY * (front + back) * 0.5);
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        return buffer.peek();
+    }
+}
